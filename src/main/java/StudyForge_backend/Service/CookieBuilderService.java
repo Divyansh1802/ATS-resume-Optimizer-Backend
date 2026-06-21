bf5fb2abfb775abf7261acb5076e9094c62c512b
Divyansh Upadhyay <divyanshupadhyay1802@gmail.com>
2026-06-21 15:18:45 +0530

    Change cookie name

--- src/main/java/StudyForge_backend/Service/CookieBuilderService.java
+++ src/main/java/StudyForge_backend/Service/CookieBuilderService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class CookieBuilderService {
 
     public Cookie create(String token){
-        Cookie cookie = new Cookie("jwt", token);
+        Cookie cookie = new Cookie("jwt_ATSresume", token);
         cookie.setPath("/");
         cookie.setMaxAge(7*24*60*60);
         cookie.setHttpOnly(true);
